9bdc5bed709398ec34d52e41f7f26f0e75c62be4
Micrasoft007 <wzx123wzxgood@outlook.com>
2024-09-15 16:43:55 -0400

    add purchase page and fix some css bug

---
+++ script/purchase-page.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // will execute when the page loads
+    document.querySelector("#order_summary").style.display = "none"
+    document.querySelector("#error").style.display = "none"
+})
