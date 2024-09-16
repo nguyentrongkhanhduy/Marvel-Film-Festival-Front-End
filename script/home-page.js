77602aca266d93c146f7732d362f4920ac054f1f
duyntk2 <duyntk2@vng.com.vn>
2024-09-16 11:39:35 -0400

    add descriptioon section

---
+++ script/home-page.js
@@ -0,0 +1,12 @@
+const Festival = {
+  name: "MARVEL STUDIOS 10th ANNIVERSARY FILM FESTIVAL",
+  date: "August 30th - September 6th",
+  address: "160 Kendal Ave, Toronto, ON M5R 1M3",
+  detail: "Exclusively in IMAX",
+};
+
+const IronMan = {};
+
+const Avengers4 = {};
+
+const Spiderman3 = {};
