058183413da9a4b676e858c0d1c5b7cb10c39af4
duyntk2 <duyntk2@vng.com.vn>
2024-09-17 09:55:30 -0400

    devide css file into specific files for more readable

--- script/home-page.js
+++ script/home-page.js
@@ -1,11 +1,9 @@
-const Festival = {
-  name: "MARVEL STUDIOS 10th ANNIVERSARY FILM FESTIVAL",
-  date: "August 30th - September 6th",
-  address: "160 Kendal Ave, Toronto, ON M5R 1M3",
-  detail: "Exclusively in IMAX",
+const IronMan = {
+  name: "IRON MAN",
+  img: "../img/iron_man_1.jpg",
+  duration: "126 minutes",
+  date: "Aug 30th",
 };
-
-const IronMan = {};
 
 const Avengers4 = {};
 
